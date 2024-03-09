e70e53d0850d90d145a69b2527addb825918e065
keshav1998 <keshav.mishra0298@gmail.com>
2024-03-09 05:58:23 +0530

    add: questions as constants

---
+++ queries/constant.py
@@ -0,0 +1,9 @@
+class Questions:
+    QUESTION_1 = "Will the Liberal Party win the most seats in the 2021 Canadian federal election?"
+    QUESTION_2 = "Will Armin Laschet be the next chancellor of Germany for 2022?"
+    QUESTION_3 = (
+        "Will NASA record 2023 as the hottest year on record? (1.03°C or higher)"
+    )
+    QUESTION_4 = "Will the price of Bitcoin (BTC) be above $46,000 on April 14, 2022?"
+    QUESTION_5 = "Will the USA report a 7-day COVID-19 case average of 200k or more before January 1, 2022?"
+    QUESTION_6 = "Formula 1: Who will finish ahead at the Saudi Arabia Grand Prix: Max Verstappen or Charles Leclerc?"
